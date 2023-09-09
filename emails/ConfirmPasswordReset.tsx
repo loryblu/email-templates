1d6785e2828eebeeaf483ed428bf2af175edbbcc
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-09-09 10:58:58 -0300

    chore: config prettier

--- emails/ConfirmPasswordReset.tsx
+++ emails/ConfirmPasswordReset.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
-import { Html, Head, Preview, Body, Container, Text, Button } from '@react-email/components';
+import {
+  Html,
+  Head,
+  Preview,
+  Body,
+  Container,
+  Text,
+  Button,
+} from '@react-email/components';
 
 interface ConfirmPasswordResetProps {
   appName: string;
@@ -28,14 +36,18 @@ export default function ConfirmPasswordReset(props: ConfirmPasswordResetProps) {
     <Html>
       <Head>
         <title>{`Mensagem | ${appName}`}</title>
-        <Preview>Recebemos uma solicitação para redefinir a senha da sua conta {appName}</Preview>
+        <Preview>
+          Recebemos uma solicitação para redefinir a senha da sua conta{' '}
+          {appName}
+        </Preview>
       </Head>
       <Body style={StyleSheet.body}>
         <Container style={StyleSheet.container}>
           <Text>Olá, {username}!</Text>
 
           <Text>
-            Pedido de redefinição de senha recebido. Clique no botão abaixo para confirmar a redefinição de senha.
+            Pedido de redefinição de senha recebido. Clique no botão abaixo para
+            confirmar a redefinição de senha.
           </Text>
 
           <Button href={url} style={StyleSheet.button} pX={16} pY={8}>
@@ -43,7 +55,8 @@ export default function ConfirmPasswordReset(props: ConfirmPasswordResetProps) {
           </Button>
 
           <Text>
-            Obrigado por nos ajudar a manter sua conta segura!<br />
+            Obrigado por nos ajudar a manter sua conta segura!
+            <br />
             Equipe {appName}.
           </Text>
         </Container>
